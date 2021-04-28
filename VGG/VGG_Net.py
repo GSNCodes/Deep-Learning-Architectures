373b27cbc325af99e84b8a84ce6a5f6137c498a2
G SowmiyaNarayanan <gsnspidey@gmail.com>
2021-04-28 19:02:41 +0530

    VGG 16 & 19

---
+++ VGG/VGG_Net.py
@@ -0,0 +1,89 @@
+import torch
+import torch.nn as nn
+
+
+class VGG(nn.Module):
+
+	def __init__(self, vgg_type, in_channels=3, num_classes=10):
+
+		super().__init__()
+		self.in_channels = in_channels
+
+		self.VGG16_architecture = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
+		self.VGG19_architecture = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M']
+		
+		if vgg_type == 16:
+			self.conv_layers = self.create_conv_layers(self.VGG16_architecture, in_channels)
+
+		elif vgg_type == 19:
+			self.conv_layers = self.create_conv_layers(self.VGG19_architecture, in_channels)
+
+		self.fcs = nn.Sequential(
+			nn.Linear(512*7*7, 4096),
+			nn.ReLU(),
+			nn.Dropout(p=0.5),
+			nn.Linear(4096, 4096),
+			nn.ReLU(),
+			nn.Dropout(p=0.5),
+			nn.Linear(4096, num_classes))
+
+	def forward(self, x):
+		x = self.conv_layers(x)
+		x = x.reshape(x.shape[0], -1)
+		x = self.fcs(x)
+
+		return x
+
+	def create_conv_layers(self, architecture, in_channels):
+		layers = []
+		batch_norm = True
+		for x in architecture:
+			if type(x) == int:
+
+				out_channels = x
+				layers += [
+							nn.Conv2d(in_channels=in_channels, 
+							out_channels=out_channels, 
+							kernel_size=3, 
+							stride=1, 
+							padding=1)
+							]
+				
+				if batch_norm:
+					layers.append(nn.BatchNorm2d(x))
+				
+				layers.append(nn.ReLU())
+
+				in_channels = x
+
+
+			elif x == 'M':
+				layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+
+		return nn.Sequential(*layers)
+
+
+def vgg16():
+
+	return VGG(16)
+
+def vgg19():
+
+	return VGG(19)
+
+if __name__ == '__main__':
+
+	model_vgg_16 = vgg16()
+	x = torch.randn(1, 3, 224, 224)
+	print("Test - VGG16")
+	print("Output Shape:", (model_vgg_16(x).shape))
+	print("Expected Shape: (with default num_classes of 10)", [1, 10])
+
+
+	model_vgg_19 = vgg19()
+	x = torch.randn(1, 3, 224, 224)
+	print("Test - VGG19")
+	print("Output Shape:", model_vgg_19(x).shape)
+	print("Expected Shape: (with default num_classes of 10)", [1, 10])
+
+
---
+++ VGG/main.py
@@ -0,0 +1,109 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data.dataloader import DataLoader
+from torch.utils.data import random_split
+
+from VGG_Net import *
+
+
+dataset = datasets.CIFAR10(root='data/', download=True, transform=ToTensor())
+test_dataset = datasets.CIFAR10(root='data/', train=False, transform=ToTensor())
+
+torch.manual_seed(43)
+val_size = 5000
+train_size = len(dataset) - val_size
+
+train_ds, val_ds = random_split(dataset, [train_size, val_size])
+print(len(train_ds), len(val_ds))
+
+batch_size=128
+train_loader = DataLoader(train_ds, batch_size, shuffle=True, num_workers=4, pin_memory=True)
+val_loader = DataLoader(val_ds, batch_size*2, num_workers=4, pin_memory=True)
+test_loader = DataLoader(test_dataset, batch_size*2, num_workers=4, pin_memory=True)
+
+
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+
+class ImageClassificationBase(nn.Module):
+    def training_step(self, batch):
+        images, labels = batch 
+        out = self(images)                  # Generate predictions
+        loss = F.cross_entropy(out, labels) # Calculate loss
+        return loss
+    
+    def validation_step(self, batch):
+        images, labels = batch 
+        out = self(images)                    # Generate predictions
+        loss = F.cross_entropy(out, labels)   # Calculate loss
+        acc = accuracy(out, labels)           # Calculate accuracy
+        return {'val_loss': loss.detach(), 'val_acc': acc}
+        
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+    
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], val_loss: {:.4f}, val_acc: {:.4f}".format(epoch, result['val_loss'], result['val_acc']))
+
+def evaluate(model, val_loader):
+    outputs = [model.validation_step(batch) for batch in val_loader]
+    return model.validation_epoch_end(outputs)
+
+def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
+    history = []
+    optimizer = opt_func(model.parameters(), lr)
+    for epoch in range(epochs):
+        # Training Phase 
+        for batch in train_loader:
+            loss = model.training_step(batch)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+        # Validation phase
+        result = evaluate(model, val_loader)
+        model.epoch_end(epoch, result)
+        history.append(result)
+    return history
+
+def get_default_device():
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+class DeviceDataLoader():
+    """Wrap a dataloader to move data to a device"""
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+        
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl: 
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
+
+
+train_loader = DeviceDataLoader(train_loader, device)
+val_loader = DeviceDataLoader(val_loader, device)
+test_loader = DeviceDataLoader(test_loader, device)
